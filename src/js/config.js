ccc0c960bfd7691dc49f2833c916bfe06b5d6909
OmarZahrah <omar.zahrah01@gmail.com>
2023-07-10 21:07:19 +0300

    changed key

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '5e7c99ca-b2e4-4876-9f80-8a5adf9e57d8';
+export const KEY = '57ec637f-5a62-4d0f-8cb6-7e4b625c40f5';
 export const MODAL_CLOSE_SEC = 2.5;
